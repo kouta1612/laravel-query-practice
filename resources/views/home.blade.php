ce43d964c043dc3c59bf96198d496f0cb695f1dd
kouta1612 <kouta1612world69@gmail.com>
2021-02-21 18:27:15 +0900

    配列でやってみた

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,9 +7,15 @@
 </head>
 
 <body>
-    @foreach ($users->unique('name') as $user)
+    {{-- @foreach ($users->unique('name') as $user)
         <p>ユーザ名：{{ $user->name }}</p>
         <p>役割：{{ $users->where('name', $user->name)->pluck('role_name')->join(', ') }}</p>
+    @endforeach --}}
+    @foreach ($users->toArray() as $user)
+        <p>ユーザ名：{{ $user['name'] }}</p>
+        <p>ロール名：{{ implode(', ', array_column(array_filter($users->toArray(), function ($element) use ($user) {
+            return $element['name'] == $user['name'];
+        }), 'role_name')) }}</p>
     @endforeach
     <p>{{ $users }}</p>
 </body>
